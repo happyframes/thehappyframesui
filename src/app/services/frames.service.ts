516dbc34c2a5a410281d312ae151e90598c70134
ramiredy12345 <ramireddy1774@gmail.com>
2022-08-10 18:05:16 +0530

    the happy frames

---
+++ src/app/services/frames.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FramesService {
+
+  constructor(private readonly apiService: ApiService) { }
+  login(payload: any): Observable<any> {
+    const url = 'loginApp/register/';
+    return this.apiService.post(url, payload);
+  }
+}
